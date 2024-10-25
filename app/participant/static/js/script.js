105b68d69d97d2d7d7a1580472c54dd9fac4c791
zockerei <2paulengelhardt@hitler.rocks>
2024-10-25 09:58:17 +0200

    Added snowflakes to the dashboard. Button now to the right. Button switched in edit_message.

--- app/participant/static/js/script.js
+++ app/participant/static/js/script.js
@@ -1,31 +1,3 @@
-// Add a simple snowfall effect
-function createSnowflakes() {
-    const snowflakeContainer = document.querySelector('.snowflake');
-    for (let i = 0; i < 100; i++) {
-        const snowflake = document.createElement('div');
-        snowflake.className = 'snowflake-item';
-        snowflake.style.left = Math.random() * 100 + 'vw';
-        snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
-        snowflakeContainer.appendChild(snowflake);
-    }
-}
-
-// Call the function when the page loads
-window.onload = function() {
-    playChristmasMusic();
-    createSnowflakes(); // Initialize snowflakes
-};
-
-// Optionally, add a toggle for the music
-document.getElementById('music-toggle').addEventListener('click', function() {
-    const audio = document.querySelector('audio');
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
-});
-
 // Function to view message
 function viewMessage(receiverId, year, receiverName) {
     fetch(`/participant/view_message/${receiverId}/${year}`)
@@ -53,8 +25,7 @@ function viewMessage(receiverId, year, receiverName) {
 // Function to confirm delete
 function confirmDelete(messageId) {
     var deleteForm = document.getElementById('deleteForm');
-    var baseUrl = "{{ url_for('participant.delete_message', message_id=0) }}";
-    deleteForm.action = baseUrl.replace('0', messageId);
+    deleteForm.action = `/participant/delete_message/${messageId}`;
     $('#deleteModal').modal('show');
 }
 
