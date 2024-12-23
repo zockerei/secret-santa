c818c2308cf3a972847e4268f8a21f8487ed4d1d
random <2paulengelhardt@hitler.rocks>
2024-12-23 13:58:05 +0100

    Created db models in sqlmodel. Added database connection (sqlite test).

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,14 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from database import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
